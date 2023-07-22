3b0832575e5b554048261c791f43d6a22646ff1b
Abhay Sharma <abhay.shar98@gmail.com>
2023-07-22 21:45:46 +0530

    Adding new project in project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,8 @@ import cinqio from "../images/cinqio.png";
 import scifi from "../images/scifi.png";
 import cellwork from "../images/cellwork.jpg";
 import weather from "../images/weather.png";
+import portfolio from "../images/portfolio.png";
+
 
 function Projects() {
 	return (
@@ -19,7 +21,7 @@ function Projects() {
         		<div class="sm:w-7/12 pl-0 p-5">
           		<div class="space-y-2">
             			<div class="">
-              			<h4 class="text-2xl font-semibold text-cyan-900">Accountabilityindia</h4>
+              			<h3 class="text-2xl font-semibold text-cyan-900">Accountabilityindia</h3>
               			<p class="text-gray-600 pt-3">Accountability Initiative is a research group which works on strengthening 
               			   transparency and accountability in governance. This research also works with CPR india.
               			</p>
@@ -36,7 +38,7 @@ function Projects() {
         		<div class="sm:w-7/12 pl-0 p-5">
           		<div class="space-y-2">
             			<div class="">
-              			<h4 class="text-2xl font-semibold text-cyan-900">Cinqio-Me</h4>
+              			<h3 class="text-2xl font-semibold text-cyan-900">Cinqio-Me</h3>
               			<p class="pt-3 text-gray-600">a diversified business group,  The diversity of the group allows us to be active in 
               				a wide range of industries, servicing some of the largest projects in the region.
               			</p>
@@ -53,7 +55,7 @@ function Projects() {
         		<div class="sm:w-7/12 pl-0 p-5">
           		<div class="space-y-2">
             			<div class="">
-              			<h4 class="text-2xl font-semibold text-cyan-900">Sci-Fi India</h4>
+              			<h3 class="text-2xl font-semibold text-cyan-900">Sci-Fi India</h3>
               			<p class="pt-3 text-gray-600">This project(SCI-FI) & cprindia program aims to better understand ‘the governance scale’ 
               				in Indian cities in tandem with ‘sector-specific socio-economic scales.
               			</p>
@@ -69,7 +71,7 @@ function Projects() {
         		<div class="sm:w-7/12 pl-0 p-5">
           		<div class="space-y-2">
             			<div class="">
-              			<h4 class="text-2xl font-semibold text-cyan-900">Cellworks Life</h4>
+              			<h3 class="text-2xl font-semibold text-cyan-900">Cellworks Life</h3>
               			<p class="pt-3 text-gray-600">Cellworks predicts response to FDA-approved cancer therapies, 
               				& provides personalized cancer therapy recommendations.
               			</p>
@@ -84,16 +86,30 @@ function Projects() {
 
 		<div class=" pt-16 pb-8 text-center">
 		<h2 class="text-2xl text-cyan-900 font-bold md:text-4xl">My Work</h2>
-		<p class="lg:w-6/12 lg:mx-auto pt-2">Here is project I've made.</p>
+		<p class="lg:w-6/12 lg:mx-auto pt-2">Here are project's I've made.</p>
 		</div>
 
-		<div class="lg:w-6/12 m-auto">
+		<div class="grid gap-12 lg:grid-cols-2">
       	<div class=" rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+        		<img src={portfolio} alt="art cover" loading="lazy" class="h-auto w-full sm:w-5/12  rounded-lg" />
+        		<div class="sm:w-7/12 pl-0 p-5">
+          		<div class="space-y-2">
+            			<div class="">
+              			<h3 class="text-2xl font-semibold text-cyan-900">Portfolio</h3>
+              			<p class="text-gray-600 pt-3">This portfolio project is created by using React js and tailwind css.
+              			For form submission data i have used getform.io plateform in contact page.</p>
+                
+            			</div>
+            			<a href="https://abhay-port-folio.netlify.app/" target="_blank" rel="noreferrer" class="block w-max text-cyan-600">Visit Site</a>
+          		</div>
+        		</div>
+     		</div>
+     		<div class=" rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
         		<img src={weather} alt="art cover" loading="lazy" class="h-auto w-full sm:w-5/12  rounded-lg" />
         		<div class="sm:w-7/12 pl-0 p-5">
           		<div class="space-y-2">
             			<div class="">
-              			<h4 class="text-2xl font-semibold text-cyan-900">Wather App</h4>
+              			<h3 class="text-2xl font-semibold text-cyan-900">Wather App</h3>
               			<p class="text-gray-600 pt-3">Wather App is created by using (openweathermap api) Real-Time Weather Data.
               			I've created this app with pure html and js, 
               			The functionaity added in this app is user can
